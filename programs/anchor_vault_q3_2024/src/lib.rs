450e298980686000108fcb8e08adf16d09152d6e
Prashant Varma <prashantvarma5083@gmail.com>
2024-08-07 12:01:58 +0530

    feat: completed the task

--- programs/anchor_vault_q3_2024/src/lib.rs
+++ programs/anchor_vault_q3_2024/src/lib.rs
@@ -8,6 +8,7 @@ pub mod anchor_vault_q3_2024 {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         ctx.accounts.initialize(&ctx.bumps)?;
+        
         Ok(())
     }
 
@@ -19,6 +20,11 @@ pub mod anchor_vault_q3_2024 {
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
         ctx.accounts.withdraw(amount)?;
+
+        Ok(())
+    }
+      pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
 
         Ok(())
     }
@@ -53,6 +59,27 @@ impl<'info> Initialize<'info> {
     }  
 }
 
+#[derive(Accounts)]
+pub  struct Close<'info>{
+    #[account()]
+    pub user: Signer<'info>,
+       #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()], 
+        // Our program will be able sign transactions on behalf of the vault account.
+        bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+        close = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+
+}
 #[derive(Accounts)]
 pub struct Deposit<'info> {
     #[account(mut)]
@@ -89,6 +116,39 @@ impl<'info> Deposit<'info> {
         Ok(())
     }
 }
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+
+        //  transfer remaining balance from vault account to user's account
+            let cpi_accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.user.to_account_info(),
+            };
+
+            // *****  seeds and PDA are used to securely authorize a transfer of lamports from the vault account to the signer account. *****
+            //  we need seed to generate PDA
+            let seeds = &[
+                b"vault",
+                self.vault_state.to_account_info().key.as_ref(),
+                &[self.vault_state.vault_bump],
+            ];
+         
+            let pda_signer = &[&seeds[..]];
+
+            let cpi_ctx = CpiContext::new_with_signer(
+                self.system_program.to_account_info(),
+                cpi_accounts,
+                pda_signer,
+            );
+
+            let amount = self.vault.lamports(); // remaining lamports in the vault account
+
+            transfer(cpi_ctx, amount)
+    }
+
+}
+
+
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
@@ -116,7 +176,7 @@ impl<'info> Withdraw<'info> {
             from: self.vault.to_account_info(),
             to: self.user.to_account_info(),
         };
-
+       
         let seeds = &[
             b"vault",
             self.vault_state.to_account_info().key.as_ref(),
